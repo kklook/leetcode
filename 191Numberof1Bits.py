8d634e1b2e74d410017797dfba103afb025fc64e
null122 <714779883@qq.com>
2016-04-12 19:23:38 +0800

    用338的方法会超时

---
+++ 191Numberof1Bits.py
@@ -0,0 +1,15 @@
+
+
+class Solution(object)
+    
+    def hammingWeight(self, n)
+        
+        type n int
+        rtype int
+        
+        sum = 0
+        while(n  0)
+            if n%2 == 1
+                sum += 1
+            n = 2
+        return sum
