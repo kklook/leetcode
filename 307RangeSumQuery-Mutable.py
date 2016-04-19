3b27e8d4ed9d713664cacc15aa23efb49e083013
null122 <714779883@qq.com>
2016-04-19 19:36:57 +0800

    307

---
+++ 307RangeSumQuery-Mutable.py
@@ -0,0 +1,30 @@
+
+
+#666
+class NumArray(object):
+    
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.nums = nums
+        
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        self.nums[i] = val
+        return self.nums[i]
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return sum(self.nums[i:j+1])
+        
