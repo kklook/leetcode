89a29b1b05afcfe61820d90a0b4d250d3af22419
null122 <714779883@qq.com>
2016-06-02 12:48:35 +0800

    283

---
+++ 283MoveZeroes.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    
+    def moveZeroes(self, nums):
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
